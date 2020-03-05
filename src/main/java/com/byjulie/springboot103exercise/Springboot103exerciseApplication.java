281132951c12970258d051fc631fc3dd1c2a47f6
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-04 20:21:40 -0500

    Initial commit

---
+++ src/main/java/com/byjulie/springboot103exercise/Springboot103exerciseApplication.java
@@ -0,0 +1,13 @@
+package com.byjulie.springboot103exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot103exerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot103exerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byjulie/springboot103exercise/Springboot103exerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byjulie.springboot103exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot103exerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
